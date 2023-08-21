9dfdcf4371263a9c69b453533044d1b6c48b1b06
Vinicius Drumond Duarte <viniciusdduarte@outlook.com>
2023-08-21 20:44:47 -0300

    Corrigindo lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,14 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  const animalSpecies = data.species.find(s => s.name === animal);
+  const animalSpecies = data.species.find((s) => s.name === animal);
 
   if (!animalSpecies) {
     return false;
   }
-  const animalsBelowMinAge = animalSpecies.residents.filter(animal => animal.age < age);
+  const animalsBelowMinAge = animalSpecies.residents.filter((animalData) => animalData.age < age);
 
   return animalsBelowMinAge.length === 0;
-
-}
+};
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -5,8 +5,9 @@ const getEmployeeByName = (employeeName) => {
     return {}; // Return an empty object if no parameter is provided
   }
 
-  const employee = data.employees.find(emp => emp.firstName === employeeName || emp.lastName === employeeName);
-  
+  const employee = data.employees.find((emp) => emp.firstName === employeeName
+  || emp.lastName === employeeName);
+
   return employee || {}; // Return the employee object if found, or an empty object if not found
 };
 
